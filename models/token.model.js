f059857dc88f999c4c86db3b87693235e39ef040
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-15 15:15:09 +0100

    Token  model set up

---
+++ models/token.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const tokenSchema = new mongoose.Schema(
+  {
+    refreshToken: {
+      type: String,
+      required: true,
+    },
+    ip: {
+      type: String,
+      required: true,
+    },
+    userAgent: {
+      type: String,
+      required: true,
+    },
+    isValid: {
+      type: Boolean,
+      default: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "users",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Tokens = mongoose.model("tokens", tokenSchema);
+
+module.exports = { Tokens };
