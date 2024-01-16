d4dd28e28b2ee333c0b06ad27833e341c5c3ad8c
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-16 11:21:37 +0100

    Product controller: getAllProducts & getSingleProduct

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,6 +4,39 @@ const CustomError = require("../errors");
 const path = require("path");
 
 const createProduct = async (req, res) => {
-  const product = await Product.create(req.body);
-  res.status(StatusCodes.OK).json({ product });
+  try {
+    const product = await Products.create(req.body);
+    res.status(StatusCodes.OK).json({ product });
+  } catch (error) {
+    throw new CustomError.BadRequestError(error);
+  }
+};
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Products.find({});
+    res.status(StatusCodes.OK).json({ products, count: products.length });
+  } catch (error) {
+    throw new CustomError.BadRequestError(error);
+  }
+};
+
+const getSingleProduct = async (req, res) => {
+  const { id: productId } = re.params;
+  const product = await Products.findOne({ _id: productId }).populate(
+    "reviews"
+  );
+
+  if (!product) {
+    throw new CustomError.notFoundError(
+      `NO product with this id: ${productId}`
+    );
+  }
+  res.status(StatusCodes.OK).json({ product, count });
+};
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
 };
